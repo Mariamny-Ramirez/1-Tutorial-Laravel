efc992f69685118f2a91c866e352322ca2cab3fb
mariamny <mariamnydelvalle@gmail.com>
2026-02-22 00:02:36 -0500

    Comentario agregado al producto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
  
+
 class HomeController extends Controller
 {
     public function index(): View
     {
         return view('home.index');
-    }
+    } 
 }
